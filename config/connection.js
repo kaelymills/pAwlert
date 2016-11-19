847b0a6228413829549ac5a818ceb7f044319c19
Kaely Mills <kaelymills@knights.ucf.edu>
2016-11-19 12:23:38 -0500

    create connection/orm files

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'paws_db'
+    database: 'pAwlert'
 });
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,4 @@ CREATE TABLE `pAwlert`.`users` (
   `username` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL);
+
