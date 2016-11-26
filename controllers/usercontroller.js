dd7340e71433c4b65d10b836e14c539d2c3a54c8
Jennifer Winston <winston.jennifer9@gmail.com>
2016-11-26 12:40:56 -0500

    server modify

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,5 @@
 var bcrypt = require('bcryptjs');
-var models  = require('../models');
+// var models  = require('../models/usercontroller.js');
 var express = require('express');
 var router  = express.Router();
 
--- server.js
+++ server.js
@@ -1,31 +1,59 @@
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser'); // for working with cookies
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var session = require('express-session'); 
+var methodOverride = require('method-override'); // for deletes in express
 
+
+// Our model controllers (rather than routes)
+var users_controller = require('./controllers/usercontroller');
+
+// var User = require("./models")["user"];
+// User.sync();
+
+// Express settings
+// ================
+
+// instantiate our app
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
+// override POST to have DELETE and PUT
+app.use(methodOverride('_method'));
+
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 1000 * 1000 }, resave: true, saveUninitialized: true}));
+app.use(cookieParser());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+//set up handlebars
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/usercontroller.js');
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/public/landing.html')
-app.use('/', routes);
-app.use('/find', routes);
-app.use('/create', routes);
+app.use('/users', users_controller);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 
 var PORT = process.env.PORT || 3000;
+console.log("Listening" + PORT);
+
 app.listen(PORT);
 
-console.log("app is running on port " + PORT);
+
